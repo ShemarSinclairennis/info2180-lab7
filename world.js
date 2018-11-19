41ea93d3aba0c0ea036328948de3a9a5f7230b5b
Shemar Ennis <shemar.ennis@gmail.com>
2018-11-19 12:34:01 +0000

    Searchall fucntion added

--- world.js
+++ world.js
@@ -1,3 +1,33 @@
 window.onload=function(){
+    var httpRequest;
     
+    function searchAll() {
+
+              if(httpRequest.readyState===XMLHttpRequest.DONE){
+
+                 if(httpRequest.status===200){
+
+            
+
+                    var response=httpRequest.responseText;
+
+                    var output=document.getElementById('result');
+
+            
+
+                    output.innerHTML="<h1> Search Result<h1/><br>"+response;
+
+                }
+
+                else{
+
+                 alert("There was a problem with the request");
+
+                }
+
+             }
+
+             
+
+         }
 };
