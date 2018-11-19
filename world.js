3622af539b63e40e80a092eb50e5c26e8457dac0
Shemar Ennis <shemar.ennis@gmail.com>
2018-11-19 12:50:56 +0000

    Variable added eventlistener added

--- world.js
+++ world.js
@@ -1,6 +1,42 @@
 window.onload=function(){
     var httpRequest;
     httpRequest = new XMLHttpRequest();
+    var button = document.getElementById('lookup');
+    
+    button.addEventListener('click',function(element) {
+
+        element.preventDefault();
+
+        
+
+        var checked=document.getElementById('shell').checked;
+
+        if (checked==true) {
+
+         httpRequest.onreadystatechange = searchAll;
+
+         httpRequest.open('GET','world.php?country=all=true', true);
+
+         httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+         httpRequest.send();
+
+        } else {
+
+         var country_search=document.getElementById('country').value;
+
+         httpRequest.onreadystatechange = searchCountry;
+
+         httpRequest.open('GET','world.php?country='+country_search, true);
+
+         httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+         httpRequest.send();
+
+        }
+
+    });
+    
     
     function searchAll() {
 
