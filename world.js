871d313205a33530e2bbdbcbed16463d356d7795
Shemar Ennis <shemar.ennis@gmail.com>
2018-11-19 12:37:06 +0000

    SearchCountry function added

--- world.js
+++ world.js
@@ -30,4 +30,34 @@ window.onload=function(){
              
 
          }
+    function searchCountry() {
+
+             if(httpRequest.readyState===XMLHttpRequest.DONE){
+
+                 if(httpRequest.status===200){
+
+
+                    var response=httpRequest.responseText;
+
+                    var output=document.getElementById('result');
+
+            
+
+                    output.innerHTML="<h1> Search Result<h1/><br>"+response;
+
+                }
+
+                else{
+
+                 
+
+                 alert("There was a problem with the request");
+
+                }
+
+             }
+
+             
+
+         }
 };
