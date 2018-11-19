20dfc64c0867c7a71df52bce679e4c6be1f44e73
Shemar Ennis <shemar.ennis@gmail.com>
2018-11-19 12:22:50 +0000

    Excercise 2 php file modified

--- world.php
+++ world.php
@@ -5,9 +5,23 @@ $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
 
+try {
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}
 
+catch(PDOException $e)
+{
+    echo "Connection failed: " .$e->getMessage();
+    
+}
+
+$country=$_GET['country'];
+
+if ($country=='all=ture'){
 $stmt = $conn->query("SELECT * FROM countries");
+
+$stmt->execute();
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -16,3 +30,17 @@ foreach ($results as $row) {
   echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
 }
 echo '</ul>';
+
+}else{
+    $stmt = $conn->query("SELECT * FROM countries WHERE name='$country';");
+
+$stmt->execute();
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<ul>';
+foreach ($results as $row) {
+  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+}
+echo '</ul>';
+}
